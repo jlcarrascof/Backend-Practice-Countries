0fe12607ceda080e23c8afc968a951173735d450
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-14 02:09:45 -0400

    Create a handlers folder and an usersHandlers.js file to refactor the code

---
+++ api/src/handlers/usersHandler.js
@@ -0,0 +1,11 @@
+const getUsersHandler = (req, res) => { 
+    // Recibir la request
+    // Unificar datos
+    // Devolver la respuesta
+    // Invoca al controller ---> El handler nunca interactua con fuentes externas de info (DB, API, etc)    
+    res.status(200).send('Aquí estarán los usuarios');
+};
+
+const getDetailHandler = (req, res) => {
+    res.status(200).send('Aquí estará el detalle del usuario');
+};
--- api/src/routes/usersRouter.js
+++ api/src/routes/usersRouter.js
@@ -2,14 +2,6 @@ const { Router } = require('express');
 
 const usersRouter = Router();
 
-const getUsersHandler = (req, res) => { 
-    res.status(200).send('Aquí estarán los usuarios');
-};
-
-const getDetailHandler = (req, res) => {
-    res.status(200).send('Aquí estará el detalle del usuario');
-};
-
 usersRouter.get('/', getUsersHandler);
 
 usersRouter.get('/:id', getDetailHandler);

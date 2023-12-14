3c6199111c95535c9518b0daed1a2750ce840045
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-14 01:07:03 -0400

    Fix postsRouter.js

--- api/src/routes/postsRouter.js
+++ api/src/routes/postsRouter.js
@@ -1,9 +1,9 @@
 const { Router } = require('express');
 
-const usersRouter = Router();
+const postsRouter = Router();
 
-usersRouter.post('/users', (req, res) => {
+postsRouter.post('/users', (req, res) => {
     res.status(200).send('Crear un usuario');
 });
 
-module.exports = usersRouter;
+module.exports = postsRouter;

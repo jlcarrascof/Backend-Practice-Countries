bab85b459e8e8c6ed1304d7932865b909d244509
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-19 23:01:55 -0400

    Testing connection to PostgreSQL from my Node app using nodemon

--- api/src/db.js
+++ api/src/db.js
@@ -6,7 +6,7 @@ const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
 // const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // Example for postgres
 
 const sequelize = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DBHOST}/${DB_NAME}`,
+    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
     {logging: false} // set to console.log to see the raw SQL queries
 ); 
 

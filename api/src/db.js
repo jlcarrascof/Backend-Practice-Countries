4a3f2780ba42c6ba3b46f7cda1ac4825f517d866
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-19 22:42:10 -0400

    Create .env file and begin to coding to connect .env info in db.js

--- api/src/db.js
+++ api/src/db.js
@@ -1,4 +1,7 @@
 const {Sequelize} = require('sequelize');
+require('dotenv').config();
+
+const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
 
 // const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // Example for postgres
 

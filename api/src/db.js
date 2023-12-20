b5e206160cca4f14b33589a4e93c67efc3161c0a
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-19 22:54:49 -0400

    Finish to config db.js and export sequelize connection

--- api/src/db.js
+++ api/src/db.js
@@ -5,5 +5,11 @@ const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
 
 // const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // Example for postgres
 
-const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // Example for postgres
+const sequelize = new Sequelize(
+    `postgres://${DB_USER}:${DB_PASSWORD}@${DBHOST}/${DB_NAME}`,
+    {logging: false} // set to console.log to see the raw SQL queries
+); 
 
+module.exports = {
+  conn: sequelize
+};

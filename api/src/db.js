b079989d19be02e06517ee2ecd4763ce2ff752ab
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-20 01:22:08 -0400

    Create my table User in db.js

--- api/src/db.js
+++ api/src/db.js
@@ -1,4 +1,4 @@
-const {Sequelize} = require('sequelize');
+const {Sequelize, DataTypes} = require('sequelize');
 require('dotenv').config();
 
 const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
@@ -9,6 +9,29 @@ const sequelize = new Sequelize(
     `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
     {logging: false} // set to console.log to see the raw SQL queries
 ); 
+
+sequelize.define('User', {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    defaultValue: DataTypes.UUIDV4
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }, 
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  phone : {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  } 
+},
+  {timestamps: false}
+);
 
 module.exports = {
   conn: sequelize

bedebf9c645383ad73bfd56fb9110b5ac0fefa56
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-02 22:42:15 -0400

    Testing the id value (if is a number or a NaN) in getDetailHandler

--- api/src/handlers/usersHandlers.js
+++ api/src/handlers/usersHandlers.js
@@ -15,10 +15,10 @@ const getUsersHandler = (req, res) => {
 // id: => usa params
 const getDetailHandler = async(req, res) => {
     const { id } = req.params;
-
+    console.log(Number(id));
     try {
-        const response = await getUserById(id);
-        res.status(200).json(response);
+        //const response = await getUserById(id);
+        //res.status(200).json(response);
     } catch (error) {
         res.status(400).json({error: error.message});
     }

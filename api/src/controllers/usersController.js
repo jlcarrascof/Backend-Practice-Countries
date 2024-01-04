43f5821d902ccf45ef76089b9b7619ccad2e2686
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-03 22:08:41 -0400

    Create my controller getUserById

--- api/src/controllers/usersController.js
+++ api/src/controllers/usersController.js
@@ -1,7 +1,15 @@
+const axios = require('axios');
 const { User } = require('../db');
 
 const creatUserDB = async ( name, email, phone ) => {
     return await User.create({ name, email, phone });
 };
+
+const getUserById = async (id, source) => { 
+    const user = source === 'api' ? 
+    await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`).data : 
+    await User.findByPk(id);
+    return user;
+} 
 
 module.exports = {creatUserDB};

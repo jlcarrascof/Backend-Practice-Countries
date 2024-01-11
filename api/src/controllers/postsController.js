078d07dbfc070bb189cdd800d3fdf0c843a2f1c8
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-10 21:30:25 -0400

    Finish postsController.js creating the relationship with User

--- api/src/controllers/postsController.js
+++ api/src/controllers/postsController.js
@@ -1,7 +1,10 @@
 const { Post } = require('../db');
 
-const createPostDb = async (body, title) => {
+const createPostDb = async (body, title, userId) => {
     const post = await Post.create({ body, title });
+
+    await post.setUser(userId);
+
     return post;
 }
 

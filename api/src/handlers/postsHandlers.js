9428032ac76e21a5818268d4b2f9b4622ea08fbd
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-10 20:48:52 -0400

    Connect postHandlers.js with postsController.js

--- api/src/handlers/postsHandlers.js
+++ api/src/handlers/postsHandlers.js
@@ -1,8 +1,10 @@
+const { createPostDb } = require('../controllers/postsController');
+
 const createPostHandler = async (req, res) => { 
-    const { title, body } = req.body;
+    const { title, body, userId } = req.body;
 
     try {
-        const newPost = await createPostDb(title, body);
+        const newPost = await createPostDb(title, body, userId);
         res.status(200).json(newPost);
     } catch (error) {
         res.status(400).json({ error: error.message });
